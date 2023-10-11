24d687a5964c4fe0561c1185e77afc04b7b9b35f
Elvis Reyes <teclaelvis01@gmail.com>
2023-10-11 22:39:05 +0000

    feat: :zap: cart add basic logic

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,7 +1,26 @@
-const addToCart = async (cid, pid) => {
-    const res = await fetch(`/api/carts/${cid}/products/${pid}`, {method: 'POST'});
-    console.log(res);
-    
+// crear una función autoejecutable para ver si existe un idcart en el localstorage
+// si no existe entonces crear un carrito nuevo y guardarlo en el localstorage
+(async () => {
+    const cartId = localStorage.getItem('cartId');
+    if (!cartId) {
+        const cart = await fetch('/api/carts', { method: 'POST' });
+        const cartData = await cart.json();
+        localStorage.setItem('cartId', cartData.cartId);
+    }
+})();
+
+
+const addToCart = async (pid) => {
+    const cid = localStorage.getItem('cartId') || '';
+    const url = `/api/carts/${cid}/product/${pid}`;
+    const res = await fetch(url, {method: 'POST'});
+    if(!res.ok || res.status === 500) {
+        return alert('Error adding product to cart');
+    }
+    if(res.status === 404) {
+        return alert('Product not found');
+    }
+    console.log('Product added to cart',res.body);
     alert(`Product added to cart`);
 
 }
